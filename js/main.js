e0290674ac6c07649cbebf7deb8ecf174c8ab1b5
Nicola-Campagna <campagnanicola379@gmail.com>
2023-01-23 14:45:40 +0100

    unit tag "h2" "h3" html with js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const numberKm = prompt("Quanti km si desidera percorrere?");
 const agePerson = prompt("Quanti anni hai?");
 const infoTicket = (numberKm + "km " + agePerson + "anni");
 console.log(infoTicket);
+document.getElementById("infoTicket").innerHTML = infoTicket
 
 // costo biglietto = 0.21£ * km tot della tratta scelta
 const costTicket = (numberKm * 0.21);
@@ -21,16 +22,21 @@ if (agePerson < 17) {
     // costo biglietto per minori
     const costTicket = ((numberKm * 0.21) * 20 / 100);
     console.log(costTicket + " euro");
+    document.getElementById("costTicket").innerHTML = (costTicket + "euro");
 }
 // 
 else if (agePerson > 65) {
     // costo biglietto per over 65
     const costTicket = ((numberKm * 0.21) * 40 / 100);
     console.log(costTicket + " euro");
+    document.getElementById("costTicket").innerHTML = (costTicket + "euro");
+
 }
 // persone maggiorenni inferiore ai 65anni
 else {
     console.log(costTicket + "euro")
+    document.getElementById("costTicket").innerHTML = (costTicket + "euro");
+
 
 }
 
